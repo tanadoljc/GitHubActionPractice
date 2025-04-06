2ad53d9f0fe1a4c30fef47857b267c9b7dae5154
4Most <mossmost123@gmail.com>
2025-04-06 21:00:28 +0700

    fix:config

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
+const path = require('path');
 const crypto = require('crypto');
 const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash(algorithm == 'md4' ? 'sha256' : algorithm);
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development' // or 'production'
+};
