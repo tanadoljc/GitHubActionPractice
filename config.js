8c3fc3a189aa4fac9b08bcfb97700cda645dfd7b
4Most <mossmost123@gmail.com>
2025-04-06 20:49:39 +0700

    fix:config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const crypto = require('crypto');
 const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash=(algorithm === 'md4' ? 'sha256' : algorithm);
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == 'md4' ? 'sha256' : algorithm);
